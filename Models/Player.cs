84a393d5261062ed5395ec7eaa482d5ea6b008c4
manjusomu <manjusomu2001@gmail.com>
2024-02-06 18:22:02 +0530

    Db First

---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppPlayer.Models
+{
+    public partial class Player
+    {
+        public int PlayerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? JersyNumber { get; set; }
+        public int? Position { get; set; }
+        public string? Team { get; set; }
+    }
+}
